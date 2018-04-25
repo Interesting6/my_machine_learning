0c2cf25bceaafcbe2e481d662ab2b16ec6817f33
Treamy <cym1106515697@outlook.com>
2018-04-25 17:45:21 +0800

    Update my_logistic_regression.py
    add plot function and hasse matrix(may have some bug，waiting for solution)

--- my_logistic_regression.py
+++ my_logistic_regression.py
@@ -8,12 +8,17 @@
 """
 import numpy as np
 from numpy.linalg import norm
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
 def sigmoid(w,x):
     z =np.dot(x,w)
     return 1./(1.+np.exp(-z))
+
+def sigmoid_p(w,x):
+    z = np.dot(x, w)
+    return np.exp(-z)/((1.+ np.exp(-z))**2)
 
 def sign(x):
     if x > 0.5:
@@ -50,6 +55,8 @@ class LogisticRegression():
             if norm(gradient)< self.epsilon:
                 break
             else:
+                # hasse = self.calc_hasse(w,X,)
+                # lbd = np.dot(gradient,gradient) / np.dot(np.dot(gradient,hasse),gradient)
                 lbd = 0.001
                 w_n = w + lbd*gradient
                 if norm(w_n-w)<self.epsilon:
@@ -61,7 +68,33 @@ class LogisticRegression():
         self.iter = i
         return self
 
+    def plot2D(self,X,y):
+        n = len(X)
+        index1 = y==1
+        index0 = y==0
+        X_1 = X[index1,:]
+        X_0 = X[index0,:]
+        x1_s, x1_e = X[:, 0].min() - 0.5, X[:, 0].max()+0.5
+        x2_s, x2_e = X[:, 1].min() - 0.5, X[:, 1].max() + 0.5
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(X_1[:,0], X_1[:,1], s=30, c='red', marker='s')
+        ax.scatter(X_0[:,0], X_0[:,1], s=30, c='green')
+        X1,X2 = np.meshgrid(np.linspace(x1_s,x1_e,50),np.linspace(x2_s,x2_e,50))
+        X_ = np.vstack((X1.flatten(),X2.flatten())).T
+        Y = self.prediction4plot(X_).reshape(X1.shape)
 
+        plt.contour(X1,X2,Y,[0.5],colors="k",linewidths=1,origin="lower")
+        plt.title("Logistic Regression")
+        plt.xlabel('X1')
+        plt.ylabel('X2')
+        plt.show()
+
+    def prediction4plot(self,X):
+        n_X, n_feat = X.shape
+        X = np.hstack((np.ones((n_X, 1)), X))
+        pi_z = sigmoid(self.w, X)
+        return pi_z
 
     def prediction(self,X):
         n_X, n_feat = X.shape
@@ -89,12 +122,12 @@ def loadDataSet(path):
 if __name__ == "__main__":
     X,y = loadDataSet("testSet.txt")
     X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=0)
-    # print(X_train)
     lg = LogisticRegression()
     lg = lg.train(X_train,y_train)
+    # print(lg.iter)
     acc = lg.accurancy(X_train,y_train)
     acc2 = lg.accurancy(X_test,y_test)
     print(acc,acc2)
-
+    lg.plot2D(X,y)
 
 
