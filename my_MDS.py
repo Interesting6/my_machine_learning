ecc42e292f96d88caf4aa066bdc0c77c91ff4a7d
Treamy <cym1106515697@outlook.com>
2018-11-10 10:04:35 +0800

    Update my_MDS.py

--- my_MDS.py
+++ my_MDS.py
@@ -9,36 +9,56 @@ import pandas as pd
 from sklearn.datasets import load_iris
 
 
-datasets = load_iris()
-X = datasets['data']
-y = datasets['target']
-
-# print(datasets)
-
 def distMatrix(A, B):
     x2B = lambda x: np.sum((x - B) ** 2, 1) ** .5
     M = np.array(list(map(x2B, A)))
     return M
 
-m, d = X.shape
-D = distMatrix(X, X)
-D2 = D**2
-Di_ = np.sum(D2, 1)/m
-D_j = np.sum(D2, 0)/m
-D__ = np.sum(D2) / (m*m)
 
-B = np.zeros_like(D)
 
-for i in range(m):
-    for j in range(i, m):
-        B[i, j] = -0.5 * (D2[i, j] - Di_[i] - D_j[j] + D__)
-        B[j, i] = B[i, j]
+class myMDS(object):
+    def __init__(self, d_r = 2):
+        self.d_r = d_r
 
-e_vals, e_vecs = eig(B)
 
-d_p = 2
+    def train(self, X):
+        D = distMatrix(X, X)
+        D2 = D**2
+        Di_ = np.sum(D2, 1)/m
+        D_j = np.sum(D2, 0)/m
+        D__ = np.sum(D2) / (m*m)
 
-idx = np.argsort(e_vals, ) < 2
-vals = e_vals[idx]
-vecs = e_vecs[idx]
-X_reduction = np.dot(vals, vecs**.5)
+        B = np.zeros_like(D)
+        for i in range(m):
+            for j in range(i, m):
+                B[i, j] = -0.5 * (D2[i, j] - Di_[i] - D_j[j] + D__)
+                B[j, i] = B[i, j]
+
+        # B = np.matrix(B)
+        e_vals, e_vecs = eig(B)
+
+        idx = np.argsort(-e_vals) < self.d_r
+        vals = e_vals[idx] # 从大到小的d_r个特征值 dr*dr
+        Lambda = np.diag(vals)
+        vecs = e_vecs[:, idx] # 从大到小的d_r个特征向量(列向量) m*dr
+        X_reduction = np.dot(vecs, Lambda**.5 ) # m*dr * dr*dr = m*dr
+        # print(X_reduction.shape)
+        # print(X_reduction)
+        self.X_reduction = X_reduction
+
+
+if __name__ =='__main__':
+    datasets = load_iris()
+    X = datasets['data']
+    y = datasets['target']
+    m, d = X.shape
+
+    # print(datasets)
+    # print(X)
+
+    MDS = myMDS()
+    MDS.train(X)
+    print(MDS.X_reduction)
+
+
+
